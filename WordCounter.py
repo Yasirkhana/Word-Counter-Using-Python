e9397602833a92f52d4d2225f26c6bc8f3428b4b
Yasir Ahmad <khan10055yasir@gmail.com>
2021-05-21 08:08:33 +0500

    Final Commit
    Completed

--- WordCounter.py
+++ WordCounter.py
@@ -1,5 +1,9 @@
+#                              WORD COUNTER WITH PYTHON
+def counter(filename):                                      # Couter Function
+    with open(f"{filename}","r") as f:                      # Opening File to read
+        data = f.read()
+        new = data.split()                                  # spliting each word and saving in a list
+        #print(new)
+        print(f" Total Words in File are : {len(new)}")     #Printing the length of list (i.e total words)
 
-with open("text.txt","r") as f:
-    dataa = f.read()
-    print(dataa)
-    print(len(dataa))
+counter("bigdata.txt")                                      # Calling function 
